923964141e9dff362a1884818c6b66b704f02404
Diego Duarte <diego.duarte.ext@dasa.com.br>
2019-09-23 12:21:43 -0300

    exemplo spring boot com primefaces, acho que vou precisar disso. :(

---
+++ src/main/java/br/com/example/springbootprimefaces/SpringBootPrimefacesApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.springbootprimefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrimefacesApplication.class, args);
+	}
+
+}
